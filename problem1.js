231052220a38907e34f39049dd786fe8b6ef1838
kartik sharma <9389517814kartik@gmail.com>
2022-10-21 19:10:10 +0530

    correct the code

--- problem1.js
+++ problem1.js
@@ -1,1 +1,13 @@
 Problem 1 : Check whether a number is Prime or not
+
+let number = 14;
+
+if (number > 1) {
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            console.log(number + " is not a prime number")
+        }
+    }
+} else {
+    console.log(number + " is not a prime number");
+}
--- problem2.js
+++ problem2.js
@@ -1,1 +1,11 @@
 Problem 2 : Check whether a string is palindrome or not.
+
+var str = "racecar";
+ 
+for ( var i = 0; i < mid; i++ ) {
+    if (str[i] !== str[len - 1 - i]) {
+        console.log(str + " is not a palindrome.");
+        return;
+    }
+}
+console.log(str + " is a palindrome.");
